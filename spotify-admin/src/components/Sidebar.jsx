14d93b1b6ffd9ae9b0d87ea878a3fa33f11bd052
Jordannst <s22310272@student.unklab.ac.id>
2025-03-30 02:54:25 +0800

    add song list & delete, add some style in sidebar

--- spotify-admin/src/components/Sidebar.jsx
+++ spotify-admin/src/components/Sidebar.jsx
@@ -12,25 +12,25 @@ const Sidebar = () => {
     <div className="flex flex-col gap-5 mt-10 ">
 
       {/* add song */}
-      <NavLink to="/add-song" className="flex items-center gap-2.5 cursor-pointer text-gray-800 bg-white border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium ">
+      <NavLink to="/add-song" className="flex items-center gap-2.5 cursor-pointer text-gray-800 bg-white border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium hover:bg-gray-200 ">
         <img src={assets.add_song} className="w-5" alt="" />
         <p className="hidden sm:block">Add Song</p>
       </NavLink>
 
       {/* list song */}
-      <NavLink to="/list-song" className="flex items-center gap-2.5 cursor-pointer text-gray-800 bg-white border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium ">
+      <NavLink to="/list-song" className="flex items-center gap-2.5 cursor-pointer text-gray-800 bg-white border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium hover:bg-gray-200 ">
         <img src={assets.song_icon} className="w-5" alt="" />
         <p className="hidden sm:block">List Song</p>
       </NavLink>
 
       {/* add album */}
-      <NavLink to="/add-album" className="flex items-center gap-2.5 cursor-pointer text-gray-800 bg-white border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium ">
+      <NavLink to="/add-album" className="flex items-center gap-2.5 cursor-pointer text-gray-800 bg-white border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium hover:bg-gray-200 ">
         <img src={assets.add_album} className="w-5" alt="" />
         <p className="hidden sm:block">Add Album</p>
       </NavLink>
 
       {/* list album */}
-      <NavLink to="/list-album" className="flex items-center gap-2.5 cursor-pointer text-gray-800 bg-white border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium ">
+      <NavLink to="/list-album" className="flex items-center gap-2.5 cursor-pointer text-gray-800 bg-white border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium hover:bg-gray-200 ">
         <img src={assets.album_icon} className="w-5" alt="" />
         <p className="hidden sm:block">List Album</p>
       </NavLink>
--- spotify-admin/src/pages/ListSong.jsx
+++ spotify-admin/src/pages/ListSong.jsx
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { url } from "../App";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const ListSong = () => {
+
+    const [data, setData] = useState([]);
+
+    const fetchSongs = async () => {
+
+        try {
+
+          const response = await axios.get(`${url}/api/song/list`);
+          if (response.data.success) {
+
+              setData(response.data.songs);
+
+          } else {
+             toast.error("Error Occuured");
+          }
+
+        }catch (err) {
+
+            console.log(err);
+        }
+    }
+
+    const removeSong = async (id) => {
+      try {
+
+        const response = await axios.post(`${url}/api/song/remove`, {id});
+        if (response.data.success) {
+          toast.success(response.data.message);
+          await fetchSongs();
+        }
+
+      }catch (error) {
+          toast.error("Error Occuured");
+          console.log(error);  
+      }
+    }
+
+    useEffect(() => {
+      fetchSongs();
+    },[])
+
+
   return (
-  <div>
-  
-  </div>
+    <div>
+      <p>All Songs List</p>
+      <br />
+      <div>
+        <div className="sm:grid hidden grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Album</b>
+          <b>Duration</b>
+          <b>Action</b>
+        </div>
+        {data.map((item, index) => {
+            return (
+              <div key={index} className="grid grid-cols-[1fr_1fr_1fr] sm:grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 ">
+                <img className="w-12" src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.album}</p>
+                <p>{item.duration}</p>
+                <button onClick={() => removeSong(item._id)} className="bg-green-600 text-white px-1 py-1 mr-5 rounded cursor-pointer hover:bg-green-700 hover:scale-103 active:scale-95">Delete</button>
+              </div>
+            )
+        })}
+      </div>
+    </div>
   )
 };
 
